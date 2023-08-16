fe3cb26184fed6b219a3583384b173e595a2c29b
José Eduardo <105430849+Yoosephh@users.noreply.github.com>
2023-08-16 15:10:41 -0300

    refactor: merge

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,9 +1,15 @@
 import { Router } from "express";
+import userRouter from "./userRoutes";
 import homeRouter from "./home.routes.js";
 import postsRouter from "./posts.routes.js";
 
 const router = Router();
+
 router.use(homeRouter)
 router.use(postsRouter)
+router.use([
+    // rota para os usuario
+    userRouter
+]);
 
 export default router;
