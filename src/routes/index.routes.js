5fe0f85a6ddfea1aba65572e6401e185972cbfff
monique <moniquejp16@gmail.com>
2023-08-16 13:54:20 -0300

    feat: colocando a rota de cadastro no inde.routes, routes

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,5 +1,11 @@
 import { Router } from "express";
+import userRouter from "./userRoutes";
 
 const router = Router();
+
+router.use([
+    // rota para os usuario
+    userRouter
+]);
 
 export default router;
