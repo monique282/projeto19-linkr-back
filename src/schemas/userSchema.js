65eaec2ecab24668e394b14fd6c0d9c4be421aeb
monique <moniquejp16@gmail.com>
2023-08-17 16:36:41 -0300

    fix: tirando bugs register

--- src/schemas/userSchema.js
+++ src/schemas/userSchema.js
@@ -5,11 +5,11 @@ export const registerTable = joi.object({
     email: joi.string().email().required(),
     password: joi.string().min(3).required(),
     name: joi.string().min(1).required(),
-    photo: joi.string().required()
+    image: joi.string().uri().required()
 });
 
 export const loginTable = joi.object({
 
     email: joi.string().email().required(),
-    password: joi.string().required().min(3)
+    password: joi.string().min(3).required()
 });
