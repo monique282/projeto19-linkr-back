1ebd347f22f5001ecb2cc597cc29935b5ed6190e
pablodru <pdrocha03@gmail.com>
2023-08-18 17:35:00 -0300

    test: merge

--- src/repositories/posts.repository.js
+++ src/repositories/posts.repository.js
@@ -5,5 +5,42 @@ export async function selectSessionsByToken(token) {
 }
 
 export async function createPost(url, content, userId) {
-  return await db.query(`INSER INTO posts (url, content, "userId") VALUES ($1, $2, $3)`, [url, content, userId])
+  return await db.query(`INSERT INTO posts (url, content, "userId") VALUES ($1, $2, $3) RETURNING posts.id`, [url, content, userId])
+}
+
+export async function selectPostById(postId) {
+  return await db.query(`SELECT * FROM posts WHERE id = $1`, [postId])
+}
+
+export async function insertLike(userId, postId){
+  return await db.query(`INSERT INTO likes (userId, postId) VALUES ($1, $2)`, [userId, postId])
+}
+export async function removeLike(userId, postId){
+  return await db.query(`DELETE FROM likes WHERE userId = $1 AND postId = $2`, [userId, postId])
+}
+
+export async function sendPosts(){
+  return await db.query(`SELECT 
+  users.id AS "userId",
+  users.name AS name,
+  users.image AS image,
+  posts.id AS "postId",
+  posts.content AS content,
+  posts.url AS url,
+  COUNT(likes."userId") AS "numberLikes"
+  FROM posts
+  JOIN users ON posts."userId" = users.id
+  LEFT JOIN likes ON likes."postId" = posts.id
+  GROUP BY "userId", users.name, users.image, "postId", posts.content, posts.url
+  ORDER BY "postId" DESC
+  LIMIT 20`)
+}
+
+export async function insertHashtags(values) {
+  const query = `
+    INSERT INTO hashtags (name, "postId") VALUES ${values.map((_, index) => `($${index * 2 + 1}, $${index * 2 + 2})`).join(', ')}
+  `;
+
+  const queryParams = values.flat();
+  await db.query(query, queryParams);
 }
--- src/schemas/main.schemas.js
+++ src/schemas/main.schemas.js
@@ -4,3 +4,9 @@ export const postSchema = Joi.object({
   url: Joi.string().uri({ scheme: ['http', 'https'] }).required(),
   content: Joi.string()
 })
+
+export const likeSchema = Joi.object({
+  postId: Joi.number().integer().required(),
+  userId: Joi.number().integer().required(),
+  isLiked: Joi.boolean().required()
+})
