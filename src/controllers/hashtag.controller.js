54b30da7bb009364cd9a4e0d9e40b365e409b9fa
pablodru <pdrocha03@gmail.com>
2023-08-21 16:33:43 -0300

    feat: hashtag likes route

--- src/controllers/hashtag.controller.js
+++ src/controllers/hashtag.controller.js
@@ -1,4 +1,5 @@
 import { getHashtagsDB, getOneHashtagDB } from "../repositories/hashtag.repository.js";
+import { likesHashtagDB } from "../repositories/likes.repository.js";
 
 export async function getHashtags ( req, res ) {
     const { user } = res.locals;
@@ -24,6 +25,19 @@ export async function getOneHashtag ( req, res ) {
         if( hashtagPosts.rowCount === 0 ) return res.status(404).send({message: "Nenhum post com esta hashtag"});
 
         return res.status(200).send(hashtagPosts.rows);
+
+    } catch (err) {
+        return res.status(500).send(err.message);
+    }
+}
+
+export async function getLikesByHashtag ( req, res ) {
+    const { hashtag } = req.params;
+    try {
+
+        const hashtagsLikes = await likesHashtagDB(hashtag);
+
+        return res.status(200).send(hashtagsLikes.rows);
 
     } catch (err) {
         return res.status(500).send(err.message);
