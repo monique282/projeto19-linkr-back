bfc5dd4d698d992ed534e10662c5477c992c8b07
José Eduardo <105430849+Yoosephh@users.noreply.github.com>
2023-08-24 19:30:49 -0300

    finish merge

--- src/repositories/posts.repository.js
+++ src/repositories/posts.repository.js
@@ -93,46 +93,6 @@ export async function sendPosts(userId) {
   return result.rows;
 }
 
-<<<<<<< HEAD
-export async function sendReposts() {
-  return db.query(`
-SELECT 
-  posts.id AS "postId",
-  posts.content AS content,
-  posts.url AS url,
-  users.id AS "userId",
-  users.name AS name,
-  users.image AS image,
-  repost_user.name AS "repostedBy",
-  reposts."userId" AS "repostedId",
-  reposts."createdAt" AS "createdAt",
-  COALESCE(likes."numberLikes", 0) AS "numberLikes",
-  COALESCE(comments."numberComments", 0) AS "numberComments",
-  COALESCE(reposts."numberReposts", 0) AS "numberReposts",
-  ARRAY_AGG(likes."userId") AS "likedUserIds"
-FROM posts
-LEFT JOIN users ON users.id = posts."userId"
-LEFT JOIN (
-  SELECT "postId", COUNT(*) AS "numberLikes", "userId"
-  FROM likes
-  GROUP BY "postId", "userId"
-) AS likes ON posts.id = likes."postId"
-LEFT JOIN (
-  SELECT "postId", COUNT(*) AS "numberComments"
-  FROM comments
-  GROUP BY "postId"
-) AS comments ON posts.id = comments."postId"
-LEFT JOIN (
-  SELECT "postId", COUNT(*) AS "numberReposts", "userId", "createdAt"
-  FROM reposts
-  GROUP BY "postId", "userId", "createdAt"
-) AS reposts ON posts.id = reposts."postId"
-LEFT JOIN users AS repost_user ON reposts."userId" = repost_user.id
-GROUP BY users.id, posts.id, posts.content, posts.url, posts."createdAt", users.name, users.image, reposts."createdAt", 
-likes."numberLikes", comments."numberComments", reposts."numberReposts", repost_user.name, reposts."userId"
-ORDER BY posts.id DESC
-LIMIT 10;`);
-=======
 export async function sendReposts(userId) {
   const result = await db.query(`
     SELECT 
@@ -174,7 +134,6 @@ export async function sendReposts(userId) {
     LIMIT 10;
   `, [userId]);
   return result.rows
->>>>>>> 0ccd073b478eadfafa719ba46d6966724233dd8b
 }
 
 
